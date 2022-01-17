b1e457af13d1f025089f25146a400327e47dd866
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-01-17 07:15:24 -0800

    completed 411

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQFpO5muOOHGQA/profile-displayphoto-shrink_200_200/0/1577374845108?e=1648080000&v=beta&t=PuxWlqxqnJMznvLGPUI7IPXWGVZ4G-CAcSNY2FuAQi0" />
 
       <Card
         name={contacts[0].name}
